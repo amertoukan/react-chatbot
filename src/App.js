28489464ffd9537230ae21eb30272c1da5b90825
Amer <toukan.amer@gmail.com>
2019-02-04 15:48:28 -0500

    chatbot now is now online

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {ChatBot} from 'aws-amplify-react';
 import './App.css';
+
+ const handleComplete = (err, confirmation) => {
+  if(err){
+    alert('Bot conversation failed :(')
+  }
+    alert ('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Reservation booked. Thank you! What would you like to do next?';
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+
+return (
+  <div className="App">
+  
+  <ChatBot 
+    title = "My React Bot"
+    botName = "BookTripMOBILEHUB"
+    welcomeMessage = "Welcome, how can I help you today?"
+    onComplete = {handleComplete}
+    clearOnComplete = {true}
+/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//AWS Import
+import config from './aws-exports';
+import Amplify from 'aws-amplify';
+Amplify.configure(config)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
